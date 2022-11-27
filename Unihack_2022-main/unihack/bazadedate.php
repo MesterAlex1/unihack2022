1fa9d78dc3ad0ecf9e710408c952df88ddc4bf1c
MesterAlex1 <119063686+MesterAlex1@users.noreply.github.com>
2022-11-27 02:51:49 +0200

    Add files via upload

--- Unihack_2022-main/unihack/bazadedate.php
+++ Unihack_2022-main/unihack/bazadedate.php
@@ -1,3 +1,60 @@
-<?php
-$sql = "CREATE USER \'bredor\'@\'localhost\' IDENTIFIED VIA mysql_native_password USING \'***\';GRANT ALL PRIVILEGES ON *.* TO \'bredor\'@\'localhost\' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;CREATE DATABASE IF NOT EXISTS `bredor`;GRANT ALL PRIVILEGES ON `bredor`.* TO \'bredor\'@\'localhost\';";
-?>
+
+    <?php    
+        
+    while($row = mysql_fetch_object($result)){    
+        
+        
+    ?>  
+        <tr>  
+            <td>  
+                <?php echo $row->id;?>  
+            </td>  
+            <td>  
+                <?php echo $row->fname;?>  
+            </td>  
+            <td>  
+                <?php echo $row->mname;?>  
+            </td>  
+            <td>  
+                <?php echo $row->lname;?>  
+            </td>  
+            <td>  
+                <?php echo $row->pwd;?>  
+            </td>  
+            <td>  
+                <?php echo $row->cnf;?>  
+            </td>  
+            <td>  
+                <?php echo $row->mail;?>  
+            </td>  
+            <td>  
+                <?php echo $row->number;?>  
+            </td>  
+            <td>G  
+                <?php echo $row->sex;?>  
+            </td>  
+            <td>  
+                <?php echo $row->address;?>  
+            </td>  
+            <td>  
+                <?php echo $row->code;?>  
+            </td>  
+            <td>  
+                <?php echo $row->city;?>  
+            </td>  
+            <td>  
+                <?php echo $row->country;?>  
+            </td>  
+            <td>  
+                <?php echo $row->skills;?>  
+            </td>  
+            <td>  
+                <?php echo $row->attach_file;?>  
+            </td>  
+            <td> <a href="listing.php?id =     
+                <?php echo $row->id;?>" onclick="return confirm('Are You Sure')">Delete    
+            </a> | <a href="index.php?id =     
+                <?php echo $row->id;?>" onclick="return confirm('Are You Sure')">Edit    
+            </a> </td>  
+            <tr>  
+                <? php } ?>  
